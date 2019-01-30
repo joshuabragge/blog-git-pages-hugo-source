d3eb04b8cd10cc5cb52bc07e9b64b976e413ec1b
joshua.bragge@hotmail.com <joshuabragge@hotmail.com>
2019-01-29 23:18:47 -0700

    INITIAL COMMIT

---
+++ content/post/welcome-to-the-show.md
@@ -0,0 +1,8 @@
+---
+date: 2014-09-28
+title: Welcome to the show
+linktitle: welcome-to-the-show-2019-01-27
+---
+
+
+This is a test of your emergency broadcasting equipment. Please stand by...
---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying new blog...\033[0m"
+
+echo -e "\033[0;32mRunning hugo...\033[0m"
+hugo -d ../joshuabragge.github.io
+
+echo -e "\033[0;32mChanging to blog directory...\033[0m"
+cd ../joshuabragge.github.io
+
+echo -e "\033[0;32mCommit and push the new build...\033[0m"
+git add *
+git commit -am "New Blog Build (`date`)"
+git push
+
+echo -e "\033[0;32mChange back to hugo-blog...\033[0m"
+cd ../thoughts
+
+echo -e "\033[0;32mDeploy complete.\033[0m"
